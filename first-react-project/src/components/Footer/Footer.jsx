23edba7d9f6d3974475fd0ec43f37e9bbebdad28
Edoardo Monti <edoardo.monti.bo@gmail.com>
2024-03-12 14:03:50 +0100

    creaazione main component

--- first-react-project/src/components/Footer/Footer.jsx
+++ first-react-project/src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './Footer.css'
 
 export default function(){
     return <h1>Footer</h1>
--- first-react-project/src/components/Main/Main.jsx
+++ first-react-project/src/components/Main/Main.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import './Main.css'
 
 export default function Main(){
-    return <h1>My Main</h1>
+    return (
+        <div className="main">
+            <div className="container-main">
+                <h1>Fun Facts About React</h1>
+                <div className="list-container">
+                    <ul>
+                        <li>Was first released in 2013</li>
+                        <li>Was originally created by Jordan Walke</li>
+                        <li>Has well over 100K stars on GitHub</li>
+                        <li>Is maintained by Facebook</li>
+                        <li>Powers thousands of enterprise apps, including mobile apps</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
 }
